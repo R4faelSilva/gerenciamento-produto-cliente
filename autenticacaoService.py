451eda84d8aff1717561d8e227f60f1b4e395148
R4faelSilva <rafaelsilvamachado93@gmail.com>
2024-10-17 21:55:21 -0300

    Subindo projeto de gerenciamento de cadastros

--- autenticacaoService.py
+++ autenticacaoService.py
@@ -1,14 +1,19 @@
 import base64
+from cryptography.fernet import Fernet # (pip install cryptography)
+key = Fernet.generate_key()
+fernet = Fernet(key)
 
 usuarioLogin = "admin"
-senhaLogin = b'MTIz'
+senhaLogin = fernet.encrypt('123'.encode())
+
+
+# enctex = fernet.encrypt(senhaLogin.encode())
+# dectex = fernet.decrypt(enctex).decode()
 
 class Autenticacao:
 
   def autenticar(self, usuario, senha):
-    senhaCriptografada = base64.b64encode(senha.encode())
-
-    if usuario == usuarioLogin and senhaCriptografada == senhaLogin:
+    if usuario == usuarioLogin and senha == fernet.decrypt(senhaLogin).decode():
       print("Acesso permitido!")
       return True
     else:
